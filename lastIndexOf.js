8a4ae010211c67e12a708a11751f65c16f30e807
Julian <julian@juliantomlindesign.com>
2019-03-16 06:52:54 +0000

    finished the lastIndexOf.js excercise

---
+++ lastIndexOf.js
@@ -0,0 +1,14 @@
+function lastIndexOf(array, numberLast){
+  for (var i = 0; i < array.length; i++){
+    if (array[array.length - 1 - i] == numberLast){
+      return array.length - 1 - i;
+    }
+  }
+  return -1;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
